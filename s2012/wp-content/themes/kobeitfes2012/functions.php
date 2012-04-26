fa77b48b7fd2cb7aa36ac31da7907aa536178ab4
hissy <hishikawa@notnil-creative.com>
2012-04-26 12:59:56 +0900

    load jQuery from Google CDN

--- s2012/wp-content/themes/kobeitfes2012/functions.php
+++ s2012/wp-content/themes/kobeitfes2012/functions.php
@@ -110,3 +110,20 @@ class Bootstrap_Walker_Nav_Menu extends Walker_Nav_Menu {
 	}
 	
 }
+
+
+/**
+ * Enqueue scripts and styles
+ */
+function kobeitfes_scripts() {
+
+if ( !is_admin() ) {
+	
+	wp_deregister_script( 'jquery' );
+	wp_register_script( 'jquery', 'https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js' );
+	wp_enqueue_script( 'jquery' );
+
+}
+
+}
+add_action( 'wp_enqueue_scripts', 'kobeitfes_scripts', 50 );
