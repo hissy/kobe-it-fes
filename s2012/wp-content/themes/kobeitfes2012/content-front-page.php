d99bb86ec057e4383a75b7d60f855b17ee8dbeb3
hissy <hishikawa@notnil-creative.com>
2012-05-02 03:23:38 +0900

    delete page title from front page

--- s2012/wp-content/themes/kobeitfes2012/content-front-page.php
+++ s2012/wp-content/themes/kobeitfes2012/content-front-page.php
@@ -9,7 +9,6 @@
 		?>
 	</header>
 	<section>
-		<h1><?php the_title(); ?></h1>
 		<?php the_content(); ?>
 		<?php wp_link_pages( array( 'before' => '<div class="page-link">' . __( 'Pages:', 'toolbox' ), 'after' => '</div>' ) ); ?>
 		<?php edit_post_link( __( 'Edit', 'toolbox' ), '<span class="edit-link">', '</span>' ); ?>
